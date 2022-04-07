f97bb98bb5c0c43893854fef3e7a7fe73e9147b1
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-04-07 18:57:00 +0300

    express server

--- build/index.js
+++ build/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Hello there");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get("/", (req, res) => {
+    res.send(`
+        <div>
+            <h1>Hi there!</h1>
+        </div>
+    `);
+});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello there");
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send(`
+        <div>
+            <h1>Hi there!</h1>
+        </div>
+    `);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
